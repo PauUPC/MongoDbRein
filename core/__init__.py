585002c33d802c5e4d63ad50ced0cfa09b238d30
RecramOxg <recram.viper@hotmail.com>
2017-01-04 18:09:48 +0100

    iface

--- core/__init__.py
+++ core/__init__.py
@@ -1,8 +1,10 @@
 from pymongo import MongoClient
 from bson.code import Code
 from tkinter import Tk
+from tkinter import Button, Label, ttk
 from tkinter.filedialog import askopenfilename
 from tkinter.simpledialog import askstring
+from tkinter.messagebox import showinfo
 import csv
 
 MONGO_HOST = 'localhost'
@@ -15,8 +17,8 @@ MONGO_COLLECTION_RESULT_SINGLE_COUNT = 'mapreduce'
 MONGO_COLLECTION_RESULT_PAIRS = 'mapreducepairs'
 
 
+# connectem a la base de dades i auntentiquem, retorna la base de dades
 def get_mongo_db():
-    # get database and auth
     client = MongoClient(MONGO_HOST, MONGO_PORT)
     db = client[MONGO_DB]
     try:
@@ -26,17 +28,17 @@ def get_mongo_db():
     return db
 
 
+# crea una col·lecció nova donat un fitxer csv, si la col·lecció ja existeix la sobreescriu.
 def create_collection(collection):
-    # clean collection befor inserts
+    # netejem la col·lecció
     collection.remove({})
 
-    # open file dialog
-    Tk().withdraw()
+    # demanem el arxiu csv
     filename = askopenfilename()
     if not filename:
         filename = '../doc/groceries.csv'
 
-    # csv to doc and insert
+    # csv to doc i inserim
     with open(filename, 'r') as inputCsv:
         reader = csv.reader(inputCsv)
         for line in list(reader):
@@ -46,6 +48,7 @@ def create_collection(collection):
             collection.insert(document)
 
 
+# funcio que calcula i guarda en dues col·leccións noves el recompte d'items i de parelles d'items
 def map_reduce(collection):
     mapper = Code("""
     function() {
@@ -72,7 +75,7 @@ def map_reduce(collection):
     function() {
         for (var i = 0; i < this.list.length - 1; i++) {
             for (var j = i; j < this.list.length - 1; j++) {
-                emit({pair: [this.list[i], this.list[j+1]]}, {count: 1});
+                emit({A: this.list[i], B: this.list[j+1]}, {count: 1});
             }
         }
     };
@@ -92,29 +95,69 @@ def map_reduce(collection):
     print("pairs", result)
 
 
-def recount():
-    Tk().withdraw()
-    suport = askstring("Llindars", "suport en %")
-    confiança = askstring("Llindars", "confiança en %")
-    print("suport: " + suport + " confiança: " + confiança)
+# funció que a partir dels recompte d'items i el recompte de parelles d'items, donats uns llindars de
+# suport i confiança en calcula el nº de regles d'associació.
+def recount(db):
+    suport_p = askstring("Llindars", "suport en %")
+    confiança_p = askstring("Llindars", "confiança en %")
+    if not suport_p:
+        suport_p = '1'
+    if not confiança_p:
+        confiança_p = '1'
+
+    result_single_count = db[MONGO_COLLECTION_RESULT_SINGLE_COUNT]
+    result_pairs = db[MONGO_COLLECTION_RESULT_PAIRS]
+
+    count = result_pairs.count()
+    regles = 0
+
+    for doc in result_pairs.find():
+        pairCount = doc['value']['count']
+        pairA = doc['_id']['A']
+        pairB = doc['_id']['B']
+        countA = result_single_count.find_one({"_id": pairA})['value']['count']
+        print("A: " + pairA + " B: " + pairB + " CountAB: " + str(pairCount) +
+              " CountA: " + str(countA))
+        suport = pairCount / count
+        confiança = pairCount / countA
+
+        if(suport > (float(suport_p)) * 0.01) and (confiança > (float(confiança_p) * 0.01)):
+            regles += 1
+            # print("res: " + str(suport) + " " + str(confiança))
+
+    showinfo("resultat per suport: " + suport_p + "% confiança: " + confiança_p + "%",
+             "nº de regles d'associació trobades: " + str(regles))
 
 
+# core de la app
+def core(new):
+    db = get_mongo_db()
+    collection = db[MONGO_COLLECTION]
 
+    if new:
+        create_collection(collection)
+        result_single_count = db[MONGO_COLLECTION_RESULT_SINGLE_COUNT]
+        result_single_count.remove({})
+        result_pairs = db[MONGO_COLLECTION_RESULT_PAIRS]
+        result_pairs.remove({})
+        map_reduce(collection)
 
-def core():
-    recount()
-    # db = get_mongo_db()
-    # collection = db[MONGO_COLLECTION]
-    # # create_collection(collection)
-    # result_single_count = db[MONGO_COLLECTION_RESULT_SINGLE_COUNT]
-    # result_single_count.remove({})
-    # result_pairs = db[MONGO_COLLECTION_RESULT_PAIRS]
-    # result_pairs.remove({})
-    # map_reduce(collection)
-    # # cursor = result_pairs.find({})
-    # # for doc in cursor:
-    # #     print(doc)
+    recount(db)
 
 
 if __name__ == '__main__':
-    core()
+    root = Tk()
+    root.geometry("250x200")
+    root.wm_title("REIN")
+
+    w = Label(root, text="Programa per calcular les regles \n d'associació d'un corpus")
+    w.pack()
+
+    B1 = Button(root, text="Calcula amb una nova col·lecció", command=lambda: core(1))
+    B1.pack()
+
+    B2 = Button(root, text="Calcula amb la col·lecció existent", command=lambda: core(0))
+    B2.pack()
+
+    root.mainloop()
+
